fabe38e1c4af97289b890769e4f59a0195ebb2e9
blopax <pablo42clement@gmail.com>
2017-11-17 11:04:48 +0100

    repo clean

--- try1/ft_strchr.c
+++ try1/ft_strchr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pclement <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/13 18:55:34 by pclement          #+#    #+#             */
+/*   Updated: 2017/11/13 19:04:20 by pclement         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
@@ -10,7 +22,7 @@ char	*ft_strchr(const char *s, int c)
 		c_ascii = 256 + c % 256;
 	else
 		c_ascii = c % 256;
-	while (s[i] != c_ascii  && s[i] != '\0')
+	while (s[i] != c_ascii && s[i] != '\0')
 		i++;
 	if (s[i] == c_ascii)
 		return ((char *)(s + i));
